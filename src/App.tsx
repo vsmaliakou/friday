5a1ab1f50178aec7597b9177625939bcc98be45d
vsmaliakou <vsmaliakou@yandex.by>
2021-06-02 16:31:41 +0300

    CardsPacksContainer, Cards, requests

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import {RegisterContainer} from "./main/ui/components/auth/register/RegisterCont
 import {LoginContainer} from "./main/ui/components/auth/login/LoginContainer";
 import {ForgotPasswordContainer} from './main/ui/components/auth/forgotPassword/ForgotPasswordContainer';
 import {SetPasswordContainer} from "./main/ui/components/auth/setPassword/SetPasswordContainer";
-import {PacksContainer} from "./main/ui/components/packs/PacksContainer";
+import {CardsPacksContainer} from "./main/ui/components/packs/CardsPacksContainer";
 import {Cards} from "./main/ui/components/packs/cards/Cards";
 import {ProfileContainer} from "./main/ui/components/profile/ProfileContainer";
 
@@ -21,7 +21,7 @@ const App = () => {
                     <Route path='/profile' render={() => <ProfileContainer/>}/>
                     <Route path='/forgot' render={() => <ForgotPasswordContainer/>}/>
                     <Route path='/set-new-password/:token' render={() => <SetPasswordContainer/>}/>
-                    <Route path='/packs' render={() => <PacksContainer/>}/>
+                    <Route path='/packs' render={() => <CardsPacksContainer/>}/>
                     <Route path='/cards' render={() => <Cards/>}/>
                     <Route path='/404' render={() => <div>404 PAGE NOT FOUND</div>}/>
                     <Route path='*' render={() => <Redirect to={'/404'}/>}/>
--- src/main/ui/components/packs/CardsPack.tsx
+++ src/main/ui/components/packs/CardsPack.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Packs = () => {
+export const CardsPack = () => {
     return (
         <div>
             Packs
