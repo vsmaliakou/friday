0691bb9e77e19354213249d4dd659023de4a769b
Your Name <vpupkin@company.com>
2021-06-05 18:27:22 +0300

    blockFilter

---
+++ src/main/ui/components/packList/Filter.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import s from './_packListFilter.module.scss';
+
+
+
+
+export const Filter = () => {
+    return (
+        <div className={s.filter}>
+
+            <h4 className={s.filterTitle}>Show packs cards</h4>
+                                    
+            <div className={s.filterWrap}>
+                <input className={s.tab1} checked type="radio" name="tab" id="id1"/>
+                <input className={s.tab2} type="radio" name="tab" id="id2"/>                                 
+        
+                <label className={s.labelMy} htmlFor="id1">MY</label>
+                <label className={s.labelAll} htmlFor="id2">All</label>
+
+                <span className={s.filterSpan}>Number of cards</span>
+
+                <div className={s.contentMy}>
+                    sdfsdfdsf
+                </div>
+
+                <div className={s.contentAll}>
+
+                </div>
+
+            </div>
+
+            
+
+        </div>
+    )
+}
