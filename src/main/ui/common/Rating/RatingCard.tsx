72905932f3e28b2903c0862138a3f092c28891c5
Your Name <vpupkin@company.com>
2021-06-20 16:26:07 +0300

    fix Rating

--- src/main/ui/common/Rating/RatingCard.tsx
+++ src/main/ui/common/Rating/RatingCard.tsx
@@ -1,17 +1,14 @@
-
-import s from './RatingCard.module.scss'
 import React from 'react';
-import Rating from '@material-ui/lab/Rating';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import s from './_RatingCard.module.scss'
+import {Rating} from "@material-ui/lab";
+import { makeStyles } from '@material-ui/core/styles';
 
-const style = makeStyles({    
-    root: {                                     
+const style = makeStyles({
+    root: {
         color: "#3040ce",
-        
+        fontSize: "17px"
     }
-
 })
-
 
 const RatingCard = () => {
 
@@ -20,9 +17,9 @@ const RatingCard = () => {
     return (
         
             
-            <Rating className={classes.root} size="small" defaultValue={2.5} precision={0.5} />
+            <Rating className={classes.root} defaultValue={2.5} precision={0.5} />
                 
-        
+       
        
     )
 }
