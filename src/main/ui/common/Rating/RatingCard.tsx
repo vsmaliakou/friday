bb3bd60001a97847eaad390522130c7e7ba985a3
Your Name <vpupkin@company.com>
2021-06-20 15:52:40 +0300

    Rating

--- src/main/ui/common/Rating/RatingCard.tsx
+++ src/main/ui/common/Rating/RatingCard.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+
 import s from './RatingCard.module.scss'
-import {Rating} from "@material-ui/lab";
+import React from 'react';
+import Rating from '@material-ui/lab/Rating';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+
+const style = makeStyles({    
+    root: {                                     
+        color: "#3040ce",
+        
+    }
+
+})
+
 
 const RatingCard = () => {
 
+    const classes = style()
+
     return (
-        <div className={s.wrap}>
+        
             
-            <Rating/>
+            <Rating className={classes.root} size="small" defaultValue={2.5} precision={0.5} />
                 
-        </div>
+        
        
     )
 }
