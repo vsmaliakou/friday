110a2d42a20c2d0a2e534d24c38276a43294a762
vsmaliakou <vsmaliakou@yandex.by>
2021-06-20 11:58:53 +0300

    material-ui/lab

--- src/main/ui/common/links.tsx
+++ src/main/ui/common/links.tsx
@@ -7,4 +7,3 @@ export const forgot = <Link to='/forgot'>Password recovery</Link>
 export const profile = <Link to='/profile'>Profile</Link>
 export const packs = <Link to='/packs'>Packs</Link>
 export const cards = <Link to='/packs/:id'>Cards</Link>
-export const packList = <Link to='/packList'>PackList</Link>
